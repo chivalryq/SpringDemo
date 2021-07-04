70caed5cec306b17c771613497d43bca101157db
qiaozp <chivalry.pp@gmail.com>
2021-07-04 16:28:40 +0800

    add hot rebuild try read config

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,21 +1,30 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Book;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+//@RestController直接把json写入http相应
 @RestController
-@RequestMapping("/api")
+//默认映射所有的动词
+@RequestMapping(value = "/api")
 public class BookController {
 
+    @Value("${wuhan2020}")
+    String wuhan2020;
     private List<Book> books = new ArrayList<>();
 
+    //等价于@RequestMapping(method = RequestMethod.POST)
+    //RequestEntity是表示整个Http reponse。包括状态码、header和正文
     @PostMapping("/book")
     public ResponseEntity<List<Book>> addBook(@RequestBody Book book) {
+        // @RequestBody 可以将 HttpRequest body 中的 JSON 类型数据反序列化为合适的 Java 类型
         books.add(book);
         return ResponseEntity.ok(books);
     }
@@ -30,5 +39,10 @@ public class BookController {
     public ResponseEntity getBookByName(@RequestParam("name") String name) {
         List<Book> results = books.stream().filter(book -> book.getName().equals(name)).collect(Collectors.toList());
         return ResponseEntity.ok(results);
+    }
+
+    @GetMapping("/config")
+    public ResponseEntity<String> getConfig(){
+        return ResponseEntity.ok(wuhan2020);
     }
 }
